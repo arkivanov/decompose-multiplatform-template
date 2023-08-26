4e91b9af0a175d64a193824b3637c162c945eff7
Christos  Malliaridis <c.malliaridis@gmail.com>
2023-08-26 15:36:09 +0300

    Remove redundant binaries generation for compose-ui module

--- compose-ui/build.gradle.kts
+++ compose-ui/build.gradle.kts
@@ -17,15 +17,9 @@ kotlin {
         }
     }
 
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
-            baseName = "compose-ui"
-        }
-    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
 
     sourceSets {
         val commonMain by getting {
