7a95161c7a8f0b2e777e6e0efd9775dfa12205b8
Alisher <alisherqazaqbaev@gmail.com>
2024-03-16 12:33:16 +0500

    replace Parcelize with Serializable

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,7 +3,7 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 kotlin {
