4fd665bba106f7cf059d0edd662f3c1ca6723537
Arkadii Ivanov <arkann1985@gmail.com>
2024-03-16 11:04:45 +0000

    Updated Gradle to 8.4 and AGP to 8.2.0

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -18,19 +18,22 @@ kotlin {
             }
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
-            baseName = "shared" // Used in app-ios-swift
+        iosSimulatorArm64(),
+    )
+        .takeIf { "XCODE_VERSION_MAJOR" in System.getenv().keys } // Export the framework only for Xcode builds
+        ?.forEach {
+            // This `shared` framework is exported for app-ios-swift
+            it.binaries.framework {
+                baseName = "shared" // Used in app-ios-swift
 
-            export(libs.decompose.decompose)
-            export(libs.essenty.lifecycle)
+                export(libs.decompose.decompose)
+                export(libs.essenty.lifecycle)
+            }
         }
-    }
 
     sourceSets {
         val commonMain by getting {
