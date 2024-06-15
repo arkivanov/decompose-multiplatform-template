c7a048c6b687d5e6cbb74d891f50783aad4b3d09
Arkadii Ivanov <arkann1985@gmail.com>
2024-06-15 11:48:04 +0100

    Updated Kotlin to 2.0.0, Decompose to 3.1.0 and other dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.android.application).apply(false)
     alias(libs.plugins.android.library).apply(false)
     alias(libs.plugins.kotlin.android).apply(false)
+    alias(libs.plugins.kotlin.compose).apply(false)
     alias(libs.plugins.kotlin.multiplatform).apply(false)
     alias(libs.plugins.kotlin.serialization).apply(false)
     alias(libs.plugins.jetbrains.compose).apply(false)
